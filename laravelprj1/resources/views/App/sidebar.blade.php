26e38dc3d41f4ef3c9c5cfc1be09d512f908db80
DevinTannn <103755933+DevinTannn@users.noreply.github.com>
2026-04-28 13:15:04 +0700

    Update Laravel
    adu sakit kepala

---
+++ laravelprj1/resources/views/App/sidebar.blade.php
@@ -0,0 +1,7 @@
+<div class="list-group list-group-flush">
+    <a href="#" class="list-group-item list-group-item-action">Dashboard</a>
+    <a href="#" class="list-group-item list-group-item-action active">Produk</a>
+    @yield('submenu-produk')
+    <a href="#" class="list-group-item list-group-item-action">Supplier</a>
+    @yield('submenu-supplier')
+</div>
